73ec36d1846b8e203d4aad2cdbe4e568922d3dbb
viao18 <995697538@qq.com>
2022-05-12 08:30:39 +0800

    firs_commit3

--- src/main/java/Git_01.java
+++ src/main/java/Git_01.java
@@ -2,5 +2,6 @@ public class Git_01 {
     public static void main(String[] args) {
         System.out.println("Gittest");
         System.out.println("Gittest2");
+        System.out.println("Gittest3");
     }
 }
